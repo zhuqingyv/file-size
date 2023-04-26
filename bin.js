0f3fa38439a80d9478ec62ad7a9b8f692fe0561f
banna <zhuqingyu@xiaohongshu.com>
2023-04-26 17:53:15 +0800

    feat: base;

---
+++ bin.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const { filesize: fileSize } = require('filesize')
+const path = require('path');
+const [_filePath] = process.argv;
+const filePath = path.join(process.cwd(), _filePath);
+
+fs.stat(filePath, (err, stats) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const size = fileSize(stats.size, {base: 2, standard: "jedec"});
+    console.log('\033[1mSize:' + `「${size}」`);
+  }
+});
